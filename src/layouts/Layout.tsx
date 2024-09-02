601584c0fbacb5a3e7c3763132e41092383ec687
BryanGallo <bryangallo95@hotmail.com>
2024-09-02 01:09:35 -0500

    Estilizacion el layout para usar rutas

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,7 +3,14 @@ import { Outlet } from "react-router-dom";
 function Layout() {
     return (
         <>
-            <main>
+            <header className="bg-sky-800">
+                <div className="mx-auto max-w-6xl py-10">
+                    <h1 className="text-4xl font-extrabold text-white">
+                        Administrador de Productos
+                    </h1>
+                </div>
+            </header>
+            <main className="mt-10 mx-auto max-w-6xl p-10 bg-white">
                 <Outlet />
             </main>
         </>
