1b145163ae9dceabc76a1d38afef1700ab886231
BryanGallo <bryangallo95@hotmail.com>
2024-09-17 01:30:54 -0500

    Usando safeParse para validar los datos
    * importando DraftProductSchema para la estructura de la informacion
    * destructurando data para poder tranformar a numero el price
    * agregando trycatch

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,7 +1,27 @@
+import { safeParse } from "valibot";
+import { DraftProductSchema } from "../types";
+
 type ProductData = {
     [k: string]: FormDataEntryValue;
 };
 
 export default async function addProduct(data: ProductData) {
     console.log(data);
+    try {
+        const result = safeParse(DraftProductSchema, {
+            name: data.name,
+            price: Number(data.price),
+            description: data.description,
+        });
+
+        if (result.success) {
+            console.log('vamos a conectarnos con axios ');
+            
+        } else {
+            throw new Error("Datos no validos");
+        }
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
 }
