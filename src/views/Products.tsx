2e485a5672d697f27ad506ca56df8c541bcbd374
BryanGallo <bryangallo95@hotmail.com>
2024-09-23 23:32:30 -0500

    Implementando useLoaderData para retornar los productos
    * Se agrega cabecera de la tabla para mostrar productos

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getProducts } from "../services/ProductService";
 
 export async function loader() {
@@ -6,10 +6,12 @@ export async function loader() {
     console.log(products);
 
     //* Ya sea loader o action deben devolver algo a traves de un return
-    return {};
+    return products;
 }
 
 function Products() {
+    const products = useLoaderData();
+
     return (
         <>
             <div className="flex justify-between">
@@ -20,6 +22,19 @@ function Products() {
                 >
                     Agregar Producto
                 </Link>
+            </div>
+            <div className="p-2">
+                <table className="w-full mt-5 table-auto">
+                    <thead className="bg-slate-800 text-white">
+                        <tr>
+                            <th className="p-2">Producto</th>
+                            <th className="p-2">Precio</th>
+                            <th className="p-2">Disponibilidad</th>
+                            <th className="p-2">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
             </div>
         </>
     );
