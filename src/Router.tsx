fbf09f03925397203f21e8dbf2ce6083306e7100
BryanGallo <bryangallo95@hotmail.com>
2024-10-01 23:46:35 -0500

    Creando view EditProduct
    * View Incluido en la ruta de editar product en Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Products, { loader as productsLoader } from "./views/Products";
 import NewProduct, { action as newProductAction } from "./views/NewProduct";
+import EditProduct from "./views/EditProduct";
 
 export const router = createBrowserRouter([
     {
@@ -16,6 +17,11 @@ export const router = createBrowserRouter([
             {
                 path: "products/new",
                 element: <NewProduct />,
+                action: newProductAction,
+            },
+            {
+                path: "products/:id/edit",
+                element: <EditProduct />,
                 action: newProductAction,
             },
         ],
